53779468d0faa66b56c7e2a3e4e9c3da0fe2092b
David Fernández Flores <56683644+DavidFFDAW@users.noreply.github.com>
2021-02-11 23:46:18 +0100

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      './src/services/calculator.service.official.js',
+      './src/services/calculator.service.js',
       './src/services/calculator.service.spec.js',
     ],
 
